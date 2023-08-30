b92479fd88c71d8086b2f99d913f04736fdd4928
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-08-30 07:33:42 +0100

    Add initial version of firstscript.sh script

---
+++ firstscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# this's a comment
+echo "Welcome to bash script - Emmanuel"
+echo 
+echo "###################################################"
+
+
+echo "The following are the information about this system:"
+echo 
+
+echo "###################################################"
+echo "The system uptime is:"
+uptime
+echo 
+
+echo "###################################################"
+echo "Memory utilization is:"
+free -m
+echo 
+
+echo "###################################################"
+echo "Disc Utilization is:"
+df -h
+echo 
