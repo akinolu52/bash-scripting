5b835bc2e0016f0be087a7a52fd13c2e61923c53
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-08-30 10:13:21 +0100

    Add system information to commands.sh

---
+++ commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Welcome $USER on $HOSTNAME."
+echo "#######################################################"
+
+FREERAM=$(free -m | grep Mem | awk '{print $4}')
+LOAD=`uptime | awk '{print $9}'`
+ROOTFREE=$(df -h | grep '/dev/sda1' | awk '{print $4}')
+
+echo "#######################################################"
+echo "Available free RAM is $FREERAM MB"
+echo
+
+echo "#######################################################"
+echo "Current Load Average $LOAD"
+echo
+
+echo "#######################################################"
+echo "Free ROOT partiotion size is $ROOTFREE"
+echo
