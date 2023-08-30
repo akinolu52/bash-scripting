4a9d544a5a7a4d1f7900af99336f93648621fae2
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2023-08-30 10:44:49 +0100

    Add user input validation for skill and username

---
+++ 5_user_input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter your skill"
+read SKILL
+
+echo 
+echo "Your skill is $SKILL, and it's in high demand"
+echo
+
+read -p "Username: " USR
+read -sp "Password: " PSD
+
+echo
+echo "Welcome! Your username is $USR."
